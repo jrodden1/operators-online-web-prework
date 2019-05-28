4314208cbc72277e1ba59f0f0e820f95a4767114
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-28 15:44:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
+  if speed > 60
+    return true
+  end
 
+  if speed < 40
+    return true
+  end
+
+  if speed > 40 && speed < 60
+    return false
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
